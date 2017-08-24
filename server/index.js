76215a593d25d4e69e1349fef50507aa8154d566
Fabian Montealegre <fabian.montealegre@gorillalogic.com>
2017-08-24 09:06:38 -0600

    Added User controller

--- server/index.js
+++ server/index.js
@@ -6,12 +6,18 @@ const logger = require('./logger');
 const argv = require('minimist')(process.argv.slice(2));
 const setup = require('./middlewares/frontendMiddleware');
 const isDev = process.env.NODE_ENV !== 'production';
-const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
+const ngrok =
+  (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel
+    ? require('ngrok')
+    : false;
 const resolve = require('path').resolve;
 const app = express();
 
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 // app.use('/api', myApi);
+
+const UserController = require('./user/UserController');
+app.use('/users', UserController);
 
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
