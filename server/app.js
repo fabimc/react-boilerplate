3c3aa100af479f7d3474b76d693d00b7415b28c6
Fabian Montealegre <fabian.montealegre@gorillalogic.com>
2017-08-28 11:13:22 -0600

    Removed server stuff to another project

--- server/app.js
+++ server/app.js
@@ -3,10 +3,4 @@
 const express = require('express');
 
 const app = express();
-const db = require('./db');
-
-db.connect();
-
-const UserController = require('./user/UserController');
-app.use('/api/users', UserController);
 module.exports = app;
